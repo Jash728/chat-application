381fede979c5ce1d14fed40bc16b33444b1066aa
Jash <shahjash2809@gmail.com>
2024-08-04 02:43:45 +0530

    add tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,16 @@
+import Chat from "./components/chat/Chat"
+import Detail from "./components/detail/Detail"
+import List from "./components/list/List"
+
 const App = () => {
   return (
-    <div className=''>App</div>
+    <div className="w-[80vw] h-[90vh] bg-[rgba(17,25,40,0.75)] backdrop-blur-[19px] backdrop-saturate-[180%] rounded-[12px] border border-[rgba(255,255,255,0.125)] flex ">
+
+      <List />
+      <Chat/>
+      <Detail/>
+
+    </div>
   )
 }
 
---
+++ src/components/list/List.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const List = () => {
+  return (
+    <div className='flex-2 border-l-[1px] border-l-[#dddddd35] border-r-[1px] border-r-[#dddddd35] h-full flex flex-col'>List</div>
+  )
+}
+
+export default List
